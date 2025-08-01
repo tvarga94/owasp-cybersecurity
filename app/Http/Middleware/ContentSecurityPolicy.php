5079c2bdd07d825b49050ec1a03d3bcd90749013
tvarga94 <vargata1@gmail.com>
2025-08-01 18:49:15 +0200

    added security measures

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -12,20 +12,25 @@ class ContentSecurityPolicy
     {
         $response = $next($request);
 
-        $csp = [
-            "default-src 'self'",
-            "script-src 'self' https://cdn.jsdelivr.net https://unpkg.com",
-            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net https://cdn.jsdelivr.net https://unpkg.com",
-            "font-src 'self' https://fonts.gstatic.com https://fonts.bunny.net",
-            "img-src 'self' data:",
-            "connect-src 'self'",
-            "frame-ancestors 'none'",
-            "object-src 'none'",
-            "base-uri 'self'",
-            'report-uri /csp-report',
-        ];
+        if ($request->is('api/documentation')) {
+            $csp = "default-src 'self'; script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://unpkg.com; style-src 'self' 'unsafe-inline';";
+            $response->headers->set('Content-Security-Policy', $csp);
+        } else {
+            $cspArray = [
+                "default-src 'self'",
+                "script-src 'self' https://cdn.jsdelivr.net https://unpkg.com",
+                "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net https://cdn.jsdelivr.net https://unpkg.com",
+                "font-src 'self' https://fonts.gstatic.com https://fonts.bunny.net",
+                "img-src 'self' data:",
+                "connect-src 'self'",
+                "frame-ancestors 'none'",
+                "object-src 'none'",
+                "base-uri 'self'",
+                'report-uri /csp-report',
+            ];
+            $response->headers->set('Content-Security-Policy', implode('; ', $cspArray));
+        }
 
-        $response->headers->set('Content-Security-Policy', implode('; ', $csp));
         $response->headers->set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload');
         $response->headers->set('X-Frame-Options', 'DENY');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
